55518c94a36ea6b37fadeec63a603857e4d677ec
Harvey Francois <har.francois@gmail.com>
2022-03-06 14:08:55 -0800

    code challenge 12

--- javascript/code-challenges/challenges-11.test.js
+++ javascript/code-challenges/challenges-11.test.js
@@ -21,7 +21,7 @@ Becomes:
 function transformToLis(obj){
   let arr = [];
   for( const keyVal in obj)
-    arr.push(`<li> ${keyVal}: ${obj[keyVal]}</li>`);
+    arr.push(`<li>${keyVal}: ${obj[keyVal]}</li>`);
   return arr;
 }
 
@@ -66,7 +66,9 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
 const divisibleByFiveTwoToThePower = (input) => {
-  // Solution code here...
+  return input.map(row => {
+    return row.filter(cell => typeof cell === 'number' && cell % 5 === 0).map(num => Math.pow(2, num));
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -132,7 +134,7 @@ let starWarsData = [{
 }];
 
 let findMaleAndFemale = (data) => {
-  // Solution code here...
+  return data.filter(char => char.gender === 'male' || char.gender === 'female').map(char => char.name).join(' and ');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -142,7 +144,7 @@ Write a function named findShortest that, given the Star Wars data from Challeng
 ------------------------------------------------------------------------------------------------ */
 
 let findShortest = (data) => {
-  // Solution code here...
+  return data.reduce((shortestSoFar, char) => Number()
 };
 
 /* ------------------------------------------------------------------------------------------------
